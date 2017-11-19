6b3df7b11f0544c89d47585191ec481bd3a4ba7c
Aleshka1715 <aleshka1715@gmail.com>
2017-11-19 17:22:44 +0200

    lesson3 task3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import './helpers/clear-console';
+'use strict'
+// import './helpers/clear-console';
+//
+// import lesson from './lesson-1';
 
-import lesson from './lesson-1';
+// First Example
+// let arr =[5, 8, 3, 87, 99, 5, 3, 5, 0];
+//
+// let sorted = arr.sort((a, b) => a - b);
+// console.log(sorted);
+
+// Second example
+//  const group = {
+//     title: "Group",
+//     students: "Anna, Nick, Tom, Vasya",
+//     showList: function () {
+//
+//         this.students.split(',').forEach(
+//             student => console.log(this.title, ' : ', student)
+//         )
+//     }
+// }
+//     group.showList();
+
+let group = {};
+  Object.assign(group, {
+      title: "Group",
+      students: "Anna, Nick, Tom, Vasya",
+      showList() {
+            this.students.split(',').forEach(
+            student => console.log(this.title, ':', student)
+        )
+    }
+})
+group.showList();
